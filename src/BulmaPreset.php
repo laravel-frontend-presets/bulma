4d39fd8f5919853010875307c3d7e95dd034dd2d
Laraveldeep <d88pak@gmail.com>
2017-07-29 13:15:32 +0800

    Changed package version

--- src/BulmaPreset.php
+++ src/BulmaPreset.php
@@ -39,7 +39,7 @@ class BulmaPreset extends Preset
     protected static function updatePackageArray(array $packages)
     {
         return [
-            'bulma' => '^0.4.3',
+            'bulma' => '^0.4.4',
         ] + Arr::except($packages, ['bootstrap-sass', 'foundation-sites', 'uikit']);
     }
 
