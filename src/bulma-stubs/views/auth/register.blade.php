8df72f35be19f2c78455140861a271795cee3b8d
Allan Alvarez Ortiz <flick36@gmail.com>
2017-08-24 14:31:04 -0500

    Update register.blade.php

--- src/bulma-stubs/views/auth/register.blade.php
+++ src/bulma-stubs/views/auth/register.blade.php
@@ -1,96 +1,120 @@
 @extends('layouts.app')
 
 @section('content')
+    <section class="hero is-primary">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Register
+                </h1>
+            </div>
+        </div>
+    </section>
 
-<section class="hero is-primary">
-    <div class="hero-body">
-        <div class="container">
-            <h1 class="title">
-                Register
-            </h1>
+    <div class="columns is-marginless is-centered">
+        <div class="column is-5">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">Register</p>
+                </header>
+
+                <div class="card-content">
+                    <form class="register-form" method="POST" action="{{ route('register') }}">
+
+                        {{ csrf_field() }}
+
+                        <div class="field is-horizontal">
+                            <div class="field-label">
+                                <label class="label">Name</label>
+                            </div>
+
+                            <div class="field-body">
+                                <div class="field">
+                                    <p class="control">
+                                        <input class="input" id="name" type="name" name="name" value="{{ old('name') }}"
+                                               required autofocus>
+                                    </p>
+
+                                    @if ($errors->has('name'))
+                                        <p class="help is-danger">
+                                            {{ $errors->first('name') }}
+                                        </p>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="field is-horizontal">
+                            <div class="field-label">
+                                <label class="label">E-mail Address</label>
+                            </div>
+
+                            <div class="field-body">
+                                <div class="field">
+                                    <p class="control">
+                                        <input class="input" id="email" type="email" name="email"
+                                               value="{{ old('email') }}" required autofocus>
+                                    </p>
+
+                                    @if ($errors->has('email'))
+                                        <p class="help is-danger">
+                                            {{ $errors->first('email') }}
+                                        </p>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="field is-horizontal">
+                            <div class="field-label">
+                                <label class="label">Password</label>
+                            </div>
+
+                            <div class="field-body">
+                                <div class="field">
+                                    <p class="control">
+                                        <input class="input" id="password" type="password" name="password"
+                                               aria-describedby="passwordHelpText" required>
+                                    </p>
+
+                                    @if ($errors->has('password'))
+                                        <p class="help is-danger">
+                                            {{ $errors->first('password') }}
+                                        </p>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="field is-horizontal">
+                            <div class="field-label">
+                                <label class="label">Confirm Password</label>
+                            </div>
+
+                            <div class="field-body">
+                                <div class="field">
+                                    <p class="control">
+                                        <input class="input" id="password-confirm" type="password"
+                                               name="password_confirmation" required>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="field is-horizontal">
+                            <div class="field-label"></div>
+
+                            <div class="field-body">
+                                <div class="field is-grouped">
+                                    <div class="control">
+                                        <button type="submit" class="button is-primary">Register</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
-</section>
-
-
-<div class="container">
-
-    <div class="columns">
-
-        <div class="column is-6 is-offset-3">
-
-            <p>
-                <br>
-                <br>
-            </p>
-
-            <form class="register-form" method="POST" action="{{ route('register') }}">
-
-                {{ csrf_field() }}
-
-                <div class="field">
-                    <label class="label">Name</label>
-
-                    <div class="control has-icons-left has-icons-right">
-                        <input class="input" id="name" type="name" name="name" value="{{ old('name') }}" required autofocus>
-                    </div>
-
-                    @if ($errors->has('name'))
-                        <p class="help is-danger">
-                            {{ $errors->first('name') }}
-                        </p>
-                    @endif
-                </div>
-
-
-                <div class="field">
-                    <label class="label">E-Mail Address</label>
-
-                    <div class="control has-icons-left has-icons-right">
-                        <input class="input" id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-                    </div>
-
-                    @if ($errors->has('email'))
-                        <p class="help is-danger">
-                            {{ $errors->first('email') }}
-                        </p>
-                    @endif
-                </div>
-
-
-                <div class="field">
-                    <label class="label">Password</label>
-
-                    <div class="control has-icons-left has-icons-right">
-                        <input class="input" id="password" type="password" name="password" required>
-                    </div>
-
-                    @if ($errors->has('password'))
-                        <p class="help is-danger">
-                            {{ $errors->first('password') }}
-                        </p>
-                    @endif
-                </div>
-
-
-                <div class="field">
-                    <label class="label">Confirm Password</label>
-
-                    <div class="control has-icons-left has-icons-right">
-                        <input class="input" id="password-confirm" type="password" name="password_confirmation" required>
-                    </div>
-                </div>
-
-                <div class="control">
-                    <button type="submit" class="button is-primary">Register</button>
-                </div>
-
-            </form>
-
-        </div>
-
-    </div>
-    
-</div>    
-
 @endsection
