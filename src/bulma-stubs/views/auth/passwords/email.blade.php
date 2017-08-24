1e111899017aad4e7366148116b363a6c4f2ff15
Allan Alvarez Ortiz <flick36@gmail.com>
2017-08-24 14:32:00 -0500

    Update email.blade.php

--- src/bulma-stubs/views/auth/passwords/email.blade.php
+++ src/bulma-stubs/views/auth/passwords/email.blade.php
@@ -3,62 +3,70 @@
 @section('content')
 
 
-<section class="hero is-primary">
-    <div class="hero-body">
-        <div class="container">
-            <h1 class="title">
-                Reset Password
-            </h1>
+    <section class="hero is-primary">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Reset Password
+                </h1>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
+    <div class="columns is-marginless is-centered">
+        <div class="column is-5">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">Reset Password</p>
+                </header>
 
-
-<div class="container">
-
-    <div class="columns">
-
-        <div class="column is-6 is-offset-3">
-
-            <p>
-                <br>
-                <br>
-            </p>
-
-            @if (session('status'))
-                <div class="notification">
-                    {{ session('status') }}
-                </div>
-            @endif
-
-            <form class="forgot-password-form" method="POST" action="{{ route('password.email') }}">
-
-                {{ csrf_field() }}
-
-                <div class="field">
-                    <label class="label">E-Mail Address</label>
-
-                    <div class="control has-icons-left has-icons-right">
-                        <input class="input" id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-                    </div>
-
-                    @if ($errors->has('email'))
-                        <p class="help is-danger">
-                            {{ $errors->first('email') }}
-                        </p>
+                <div class="card-content">
+                    @if (session('status'))
+                        <div class="notification">
+                            {{ session('status') }}
+                        </div>
                     @endif
+
+                    <form class="forgot-password-form" method="POST" action="{{ route('password.email') }}">
+
+                        {{ csrf_field() }}
+
+                        <div class="field is-horizontal">
+                            <div class="field-label">
+                                <label class="label">E-Mail Address</label>
+                            </div>
+
+                            <div class="field-body">
+                                <div class="field">
+                                    <p class="control">
+                                        <input class="input" id="email" type="email" name="email"
+                                               value="{{ old('email') }}" required autofocus>
+                                    </p>
+
+                                    @if ($errors->has('email'))
+                                        <p class="help is-danger">
+                                            {{ $errors->first('email') }}
+                                        </p>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="field is-horizontal">
+                            <div class="field-label"></div>
+
+                            <div class="field-body">
+                                <div class="field is-grouped">
+                                    <div class="control">
+                                        <button type="submit" class="button is-primary">Send Password Reset Link
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-
-                <div class="control">
-                    <button type="submit" class="button is-primary">Send Password Reset Link</button>
-                </div>
-
-            </form>
-
-
+            </div>
         </div>
-
     </div>
-
 @endsection
