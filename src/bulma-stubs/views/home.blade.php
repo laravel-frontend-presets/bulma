70a617bfffa7334b114e36d70cca510723bc470a
Allan Alvarez Ortiz <flick36@gmail.com>
2017-08-24 14:30:01 -0500

    Update home.blade.php

--- src/bulma-stubs/views/home.blade.php
+++ src/bulma-stubs/views/home.blade.php
@@ -1,44 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="columns is- is-marginless is-centered">
+            <div class="column is-7">
+                <nav class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            Dashboard
+                        </p>
+                    </header>
 
-<section class="hero is-primary">
-    <div class="hero-body">
-        <div class="container">
-            <h1 class="title">
-                Home
-            </h1>
+                    <div class="card-content">
+                        You are logged in!
+                    </div>
+                </nav>
+            </div>
         </div>
     </div>
-</section>
-
-<div class="container">
-
-    <div class="columns">
-
-        <div class="column is-6 is-offset-3">
-
-            <p>
-                <br>
-                <br>
-            </p>
-
-            <nav class="panel">
-
-                <p class="panel-heading">
-                    Dashboard
-                </p>
-
-                <div class="notification">
-                  You are logged in!
-                </div>
-
-            </nav>
-
-        </div>
-
-    </div>
-
-</div>
-
 @endsection
