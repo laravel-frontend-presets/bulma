d1375bdca0525f472eabb28e8002a1d4b3aa1118
Laraveldeep <d88pak@gmail.com>
2018-02-21 23:03:05 +0545

    Add script for Bulma Navbar Burger

--- src/bulma-stubs/app.js
+++ src/bulma-stubs/app.js
@@ -22,29 +22,32 @@ const app = new Vue({
 });
 
 
-require('./bulma-extensions');
-
+// Bulma NavBar Burger Script
 document.addEventListener('DOMContentLoaded', function () {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
+    
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
-
+        
         // Add a click event on each of them
         $navbarBurgers.forEach(function ($el) {
             $el.addEventListener('click', function () {
-
+                
                 // Get the target from the "data-target" attribute
                 let target = $el.dataset.target;
                 let $target = document.getElementById(target);
-
+                
                 // Toggle the class on both the "navbar-burger" and the "navbar-menu"
                 $el.classList.toggle('is-active');
                 $target.classList.toggle('is-active');
-
+                
             });
         });
     }
-
+    
 });
+
+
+
+require('./bulma-extensions');
