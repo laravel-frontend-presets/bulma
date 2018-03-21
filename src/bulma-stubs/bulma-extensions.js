b002e83b67be5142537d8c438aafc020ebcfac76
Laraveldeep <d88pak@gmail.com>
2018-03-21 23:02:25 +0545

    Fix iconpicker case for new version

--- src/bulma-stubs/bulma-extensions.js
+++ src/bulma-stubs/bulma-extensions.js
@@ -2,7 +2,7 @@
 require('bulma-extensions/bulma-accordion/dist/bulma-accordion');
 require('bulma-extensions/bulma-calendar/dist/bulma-calendar');
 require('bulma-extensions/bulma-carousel/dist/bulma-carousel');
-require('bulma-extensions/bulma-iconpicker/dist/bulma-iconPicker');
+require('bulma-extensions/bulma-iconpicker/dist/bulma-iconpicker');
 require('bulma-extensions/bulma-quickview/dist/bulma-quickview');
 require('bulma-extensions/bulma-slider/dist/bulma-slider');
 require('bulma-extensions/bulma-steps/dist/bulma-steps');
