e33ecf582a3c188e08b460f227cda7dd0824b0d9
Laraveldeep <d88pak@gmail.com>
2018-06-11 16:11:51 +0545

    Update bulma extension version

--- src/BulmaPreset.php
+++ src/BulmaPreset.php
@@ -41,7 +41,7 @@ class BulmaPreset extends Preset
     {
         return [
             'bulma' => '^0.7.1',
-            'bulma-extensions' => '^1.0.31',
+            'bulma-extensions' => '^1.1.0',
         ] + Arr::except($packages, ['bootstrap']);
     }
 
