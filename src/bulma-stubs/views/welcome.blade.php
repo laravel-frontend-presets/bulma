c6dbe8ddf8449c94f1935d53f40bd60225ed00c7
Laraveldeep <d88pak@gmail.com>
2018-06-11 16:31:41 +0545

    Update version info

--- src/bulma-stubs/views/welcome.blade.php
+++ src/bulma-stubs/views/welcome.blade.php
@@ -112,8 +112,8 @@
                 </div>
 
                 <div class="foundation_button_test">
-                    <p class="framwork_title">Bulma 0.7.1</p>
-                    <p class="framwork_title">Bulma Extension 1.1.0</p>
+                    <p class="framwork_title">Bulma v0.7.1</p>
+                    <p class="framwork_title">Bulma Extension v1.1.0</p>
 
                     <div class="block">
                         <a class="button is-primary">Primary</a>
