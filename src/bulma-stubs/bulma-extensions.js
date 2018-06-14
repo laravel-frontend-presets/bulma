af7af3bf9f8510fed330a9a17a1fd0866797deea
Laraveldeep <d88pak@gmail.com>
2018-06-14 23:52:57 +0545

    Update path

--- src/bulma-stubs/bulma-extensions.js
+++ src/bulma-stubs/bulma-extensions.js
@@ -1,9 +1,9 @@
 
-require('bulma-extensions/bulma-accordion/dist/bulma-accordion');
+require('bulma-extensions/bulma-accordion/dist/js/bulma-accordion');
 require('bulma-extensions/bulma-calendar/dist/js/bulma-calendar');
 require('bulma-extensions/bulma-carousel/dist/js/bulma-carousel');
-require('bulma-extensions/bulma-iconpicker/dist/bulma-iconpicker');
-require('bulma-extensions/bulma-quickview/dist/bulma-quickview');
-require('bulma-extensions/bulma-slider/dist/bulma-slider');
-require('bulma-extensions/bulma-steps/dist/bulma-steps');
-require('bulma-extensions/bulma-tagsinput/dist/bulma-tagsinput');
+require('bulma-extensions/bulma-iconpicker/dist/js/bulma-iconpicker');
+require('bulma-extensions/bulma-quickview/dist/js/bulma-quickview');
+require('bulma-extensions/bulma-slider/dist/js/bulma-slider');
+require('bulma-extensions/bulma-steps/dist/js/bulma-steps');
+require('bulma-extensions/bulma-tagsinput/dist/js/bulma-tagsinput');
