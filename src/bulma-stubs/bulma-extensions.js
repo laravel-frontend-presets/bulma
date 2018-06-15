6b13364a902be5c6136bd07fc71b91c31bcdf476
Laraveldeep <d88pak@gmail.com>
2018-06-15 17:22:52 +0545

    Define extenstions and instatiate

--- src/bulma-stubs/bulma-extensions.js
+++ src/bulma-stubs/bulma-extensions.js
@@ -1,9 +1,20 @@
 
-require('bulma-extensions/bulma-accordion/dist/js/bulma-accordion');
-require('bulma-extensions/bulma-calendar/dist/js/bulma-calendar');
-require('bulma-extensions/bulma-carousel/dist/js/bulma-carousel');
-require('bulma-extensions/bulma-iconpicker/dist/js/bulma-iconpicker');
-require('bulma-extensions/bulma-quickview/dist/js/bulma-quickview');
-require('bulma-extensions/bulma-slider/dist/js/bulma-slider');
-require('bulma-extensions/bulma-steps/dist/js/bulma-steps');
-require('bulma-extensions/bulma-tagsinput/dist/js/bulma-tagsinput');
+var bulmaAccordion = require('bulma-extensions/bulma-accordion/dist/js/bulma-accordion');
+var bulmaCalendar = require('bulma-extensions/bulma-calendar/dist/js/bulma-calendar');
+var bulmaCarousel = require('bulma-extensions/bulma-carousel/dist/js/bulma-carousel');
+var bulmaIconpicker = require('bulma-extensions/bulma-iconpicker/dist/js/bulma-iconpicker');
+var bulmaQuickview = require('bulma-extensions/bulma-quickview/dist/js/bulma-quickview');
+var bulmaSlider = require('bulma-extensions/bulma-slider/dist/js/bulma-slider');
+var bulmaSteps = require('bulma-extensions/bulma-steps/dist/js/bulma-steps');
+var bulmaTagsinput = require('bulma-extensions/bulma-tagsinput/dist/js/bulma-tagsinput');
+
+
+// Instantiate
+bulmaAccordion.attach();
+bulmaCalendar.attach();
+bulmaCarousel.attach();
+bulmaIconpicker.attach();
+bulmaQuickview.attach();
+bulmaSlider.attach();
+bulmaSteps.attach();
+bulmaTagsinput.attach();
