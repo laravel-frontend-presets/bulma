4d5c0909ea978d65501b531467d7bddf6a35b687
Laraveldeep <d88pak@gmail.com>
2019-12-10 13:55:14 +0545

    Comment out reference to Vue.js

--- src/bulma-stubs/app.js
+++ src/bulma-stubs/app.js
@@ -7,7 +7,7 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,11 +15,11 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
 
 
 // Bulma NavBar Burger Script
