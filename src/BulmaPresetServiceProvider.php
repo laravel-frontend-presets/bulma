8454f8b12b366ec2b55836838f77db9c33249c54
Scott Wilson <scott@saldoventures.com>
2020-05-06 12:30:27 -0700

    Fix PresetCommand

--- src/BulmaPresetServiceProvider.php
+++ src/BulmaPresetServiceProvider.php
@@ -2,7 +2,7 @@
 namespace LaravelFrontendPresets\BulmaPreset;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Foundation\Console\PresetCommand;
+use Laravel\Ui\UiCommand;
 
 class BulmaPresetServiceProvider extends ServiceProvider
 {
@@ -13,13 +13,13 @@ class BulmaPresetServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        PresetCommand::macro('bulma', function ($command) {
+        UiCommand::macro('bulma', function ($command) {
             BulmaPreset::install(false);
             $command->info('Bulma scaffolding installed successfully.');
             $command->comment('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
         });
 
-        PresetCommand::macro('bulma-auth', function ($command) {
+        UiCommand::macro('bulma-auth', function ($command) {
             BulmaPreset::install(true);
             $command->info('Bulma scaffolding with Auth views installed successfully.');
             $command->comment('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
